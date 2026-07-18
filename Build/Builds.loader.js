8404f43c4a8252af320e76804fee0bfa4c69f944
mersalmass <vijaygamingchannel2020@gmail.com>
2026-07-18 17:57:35 +0530

    new build

--- Build/Builds.loader.js
+++ Build/Builds.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 62304486,
+    wasmFileSize: 62304494,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
